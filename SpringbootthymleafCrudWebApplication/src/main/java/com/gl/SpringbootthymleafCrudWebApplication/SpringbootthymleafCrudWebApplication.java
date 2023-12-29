2e679b48ad767df9d68bce1f0f603c3e9e0fbd35
Poojabg487 <135731717+Poojabg487@users.noreply.github.com>
2023-12-29 18:19:22 +0530

    Add files via upload

---
+++ SpringbootthymleafCrudWebApplication/src/main/java/com/gl/SpringbootthymleafCrudWebApplication/SpringbootthymleafCrudWebApplication.java
@@ -0,0 +1,13 @@
+package com.gl.SpringbootthymleafCrudWebApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootthymleafCrudWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootthymleafCrudWebApplication.class, args);
+	}
+
+}
